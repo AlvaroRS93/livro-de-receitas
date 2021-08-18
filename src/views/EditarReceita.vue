6d80091fd527f1e8df1158cd16e02d49830d5f23
AlvaroRS93 <86481086+AlvaroRS93@users.noreply.github.com>
2021-08-18 11:02:59 -0300

    Add files via upload

--- src/views/EditarReceita.vue
+++ src/views/EditarReceita.vue
@@ -1,52 +1,79 @@
 <template>
   <div class="ma-5">
-    <h1 class="display-3 d-flex justify-center">{{tituloDaTela}}</h1>
-    <v-text-field
-      label="Título"
-      v-model="receita.titulo"
-    ></v-text-field>
+    <v-form ref="form">
 
-    <v-row>
-      <v-col id="boxIngredientes" class="ma-1">
-        <div class="grey white--text rounded-lg pa-3">Ingredientes</div>
+        <h1 class="display-3 d-flex justify-center">{{ tituloDaTela }}</h1>
+        <v-text-field label="Título" v-model="receita.titulo" :rules="this.regrasValidacao.titulo"></v-text-field>
 
-        <div>
-          <v-col class="ma-n1 d-flex justify-space-between">
-            <v-text-field v-model=novoIngrediente class="mr-6" label="Ingredientes"></v-text-field>
-            <v-btn small fab outlined color="primary" class="my-auto">
-              <v-icon @click="adicionarIngrediente(novoIngrediente)">mdi-plus</v-icon>
-            </v-btn>
-          </v-col>
-        </div>
+        <v-row>
+          <v-col id="boxIngredientes" class="ma-1">
+            <div class="grey white--text rounded-lg pa-3">Ingredientes</div>
 
-        <v-list v-bind:key="ingrediente" v-for="ingrediente in receita.ingredientes">
-          <v-list-item class="ma-n3" >
-            <v-col class="my-n3 d-flex justify-space-between">
-              <v-list-item-content class="primary--text my-auto">
-                {{ ingrediente }} 
-              </v-list-item-content>
-
-              <v-list-item-action >
-                <v-btn outlined fab small class="primary--text">
-                  <v-icon @click="removerIngrediente(receita.ingredientes.indexOf(ingrediente))">mdi-delete</v-icon>
+            <div>
+              <v-col class="ma-n1 d-flex justify-space-between">
+                <v-text-field
+                  v-model="novoIngrediente"
+                  class="mr-6"
+                  label="Ingredientes"
+                ></v-text-field>
+                <v-btn small fab outlined color="primary" class="my-auto">
+                  <v-icon @click="adicionarIngrediente(novoIngrediente)"
+                    >mdi-plus</v-icon
+                  >
                 </v-btn>
-              </v-list-item-action>
-            </v-col>
-          </v-list-item>
-        </v-list>        
-      </v-col>
+              </v-col>
+            </div>
 
+            <v-list
+              v-bind:key="ingrediente"
+              v-for="ingrediente in receita.ingredientes"
+            >
+              <v-list-item class="ma-n3">
+                <v-col class="my-n3 d-flex justify-space-between">
+                  <v-list-item-content class="primary--text my-auto">
+                    {{ ingrediente }}
+                  </v-list-item-content>
 
-      <v-col id="modoDePreparo" class="ma-1">
-        <div class="grey white--text rounded-lg pa-3">Modo de Preparo</div>
-        <v-textarea label="Modo de preparo" v-model="receita.modoDePreparo" auto-grow></v-textarea>
-      </v-col>         
-    </v-row>
+                  <v-list-item-action>
+                    <v-btn outlined fab small class="primary--text">
+                      <v-icon
+                        @click="
+                          removerIngrediente(
+                            receita.ingredientes.indexOf(ingrediente)
+                          )
+                        "
+                        >mdi-delete</v-icon
+                      >
+                    </v-btn>
+                  </v-list-item-action>
+                </v-col>
+              </v-list-item>
+            </v-list>
+          </v-col>
 
-    <v-row>
-      <v-col><v-btn color="ma-2 primary" block @click="confirmar">Confirmar</v-btn></v-col>
-      <v-col> <v-btn color="ma-2" block @click="voltar">Cancelar</v-btn></v-col>
-    </v-row>
+          <v-col id="modoDePreparo" class="ma-1">
+            <div class="grey white--text rounded-lg pa-3">Modo de Preparo</div>
+
+              <v-textarea
+                label="Modo de preparo"
+                v-model="receita.modoDePreparo"
+                auto-grow
+                :rules="this.regrasValidacao.modoDePreparo"
+              ></v-textarea>
+          </v-col>
+        </v-row>
+
+        <v-row>
+          <v-col
+            ><v-btn color="ma-2 primary" block @click="validar"
+              >Confirmar</v-btn
+            ></v-col
+          >
+          <v-col>
+            <v-btn color="ma-2" block @click="voltar">Cancelar</v-btn></v-col
+          >
+        </v-row>
+    </v-form>
   </div>
 </template>
 <script>
@@ -56,50 +83,59 @@ export default {
       receita: {
         titulo: "",
         ingredientes: [],
-        modoDePreparo: ""
-      },      
+        modoDePreparo: "",
+      },
       tituloDaTela: "",
       novoIngrediente: "",
       acao: this.$router.history.current.params.acao,
+
+      regrasValidacao: {
+        titulo: [v => !!v || 'O preenchimento do campo Título é obrigatório!'],
+        modoDePreparo: [v => !!v || 'O preenchimento do campo Modo de preparo é obrigatório!'],
+      }
+
+
     };
   },
-  mounted: function(){
-    
-    //var acao = this.$router.history.current.params.acao;
+  mounted: function () {
     var indice = this.$router.history.current.params.idReceita;
 
-    if (this.acao == 1){
+    if (this.acao == 1) {
       this.tituloDaTela = "Adicionar Receita";
-    } else{
+    } else {
       this.tituloDaTela = "Editar Receita";
 
       this.receita = this.$store.getters.getLista[indice];
-      
-      
     }
   },
   methods: {
-    voltar: function () {
+    voltar: function () {      
       this.$router.push("/");
     },
-    confirmar: function(){
-      if (this.acao == 1){
+    confirmar: function () {
+        
+      if (this.acao == 1) {
         this.$store.commit("adicionaReceita", this.receita);
         window.alert("Receita adicionada!");
-
-      }else if(this.acao == 2){
+      } else if (this.acao == 2) {
         this.$store.commit("alteraReceita", this.receita);
         window.alert("Receita alterada!");
-      }      
+      }
       this.$router.push("/");
     },
-    
-    adicionarIngrediente: function(ingrediente){
+
+    adicionarIngrediente: function (ingrediente) {
       this.receita.ingredientes.push(ingrediente);
       this.novoIngrediente = "";
     },
-    removerIngrediente: function(ingrediente){
+    removerIngrediente: function (ingrediente) {
       this.receita.ingredientes.splice(ingrediente, 1);
+    },
+
+    validar: function(){
+      if(this.$refs.form.validate()){
+        this.confirmar();
+      }
     }
   },
 };
